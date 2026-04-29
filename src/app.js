0490a924243a3fdcafeeef9c5f78e3c49d27cb84
unknown <likhitagovindaraj@gmail.com>
2026-04-29 20:08:54 +0400

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -5,9 +5,38 @@ const app = express();  //instance of an express js application
 //     res.send({firstName: "Likhita", lastName:"Govindaraj"});   
 // });
 
-app.get("/user", (req,res) => {                    //request handler
-    res.send({firstName: "Likhita", lastName:"Govindaraj"});   
-});
+app.get("/user", (req,res, next) => {                    //request handler
+    //get query params in request handler
+    console.log(req.query);
+    console.log("Handling the first route handler!");
+    //res.send({firstName: "Likhita", lastName:"Govindaraj"});   
+    next();
+},
+ (req,res, next) => {                    //request handler
+    //get query params in request handler
+    console.log("Handling the second route handler!");
+    //res.send("Route handler 2!");   
+    next();
+},
+ (req,res, next) => {                    //request handler
+    //get query params in request handler
+    console.log("Handling the third route handler!");
+    //res.send("Route handler 3!");   
+    next();
+},
+ (req,res, next) => {                    //request handler
+    //get query params in request handler
+    console.log("Handling the fourth route handler!");
+    //res.send("Route handler 4!");   
+    next();
+},
+ (req,res, next) => {                    //request handler
+    //get query params in request handler
+    console.log("Handling the 5th route handler!");
+    res.send("Route handler 5!");   
+    // next();
+} 
+);
 
 app.post("/user", (req,res) => {                    //request handler
     res.send("Data successfully stored to DB");   
@@ -33,6 +62,6 @@ app.delete("/user", (req,res) => {                    //request handler
 //     res.send('Namaste Liki!');   
 // });
 //we have to listen on some port to start the server
-app.listen(7777, () => {
-    console.log('Server is successfully running on port 3000');
+app.listen(8080, () => {
+    console.log('Server is successfully running on port 8080');
 });
